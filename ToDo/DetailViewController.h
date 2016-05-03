bb486ff208265d6ab24a8189c6e2265d260cc65b
Nick Perkins <nickperkins@protonmail.com>
2016-05-03 09:16:11 -0400

    Initial Commit

---
+++ ToDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  ToDo
+//
+//  Created by Nick Perkins on 5/3/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ ToDo/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  ToDo
+//
+//  Created by Nick Perkins on 5/3/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
